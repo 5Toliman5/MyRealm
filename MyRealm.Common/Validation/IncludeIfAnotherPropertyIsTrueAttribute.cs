093dee71931a86647eec2b9acd1987120d41a721
Toliman <skylake525@gmail.com>
2024-07-14 16:32:40 +0300

    common attributes

---
+++ MyRealm.Common/Validation/IncludeIfAnotherPropertyIsTrueAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRealm.Common.Validation
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class IncludeIfAnotherPropertyIsTrueAttribute : ValidationAttribute
+    {
+        public string DependentPropertyName { get; }
+
+        public IncludeIfAnotherPropertyIsTrueAttribute(string dependentPropertyName)
+        {
+            DependentPropertyName = dependentPropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var dependentPropertyInfo = validationContext.ObjectType.GetProperty(DependentPropertyName);
+            if (dependentPropertyInfo == null)
+            {
+                return new ValidationResult($"Unknown property: {DependentPropertyName}");
+            }
+
+            var dependentPropertyValue = (bool)dependentPropertyInfo.GetValue(validationContext.ObjectInstance);
+            if (dependentPropertyValue)
+            {
+                var propertyInfo = validationContext.ObjectType.GetProperty(validationContext.MemberName);
+                var propertyValue = propertyInfo.GetValue(validationContext.ObjectInstance);
+
+                if (propertyValue == null)
+                {
+                    return new ValidationResult($"The field {validationContext.DisplayName} is required when {DependentPropertyName} is true.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ MyRealm.Common/Validation/OneTrueOfAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRealm.Common.Validation
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class OneTrueOfAttribute : ValidationAttribute
+    {
+        private readonly string[] _propertyNames;
+
+        public OneTrueOfAttribute(params string[] propertyNames)
+        {
+            _propertyNames = propertyNames;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var properties = validationContext.ObjectType.GetProperties()
+                .Where(p => _propertyNames.Contains(p.Name))
+                .ToList();
+
+            var trueCount = properties.Count(p => (bool)p.GetValue(validationContext.ObjectInstance));
+
+            if (trueCount == 1)
+            {
+                return ValidationResult.Success;
+            }
+            return new ValidationResult($"Only one of the following properties can be true: {string.Join(", ", _propertyNames)}");
+        }
+    }
+}
